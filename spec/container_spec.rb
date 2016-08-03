e5e5581bed879209a06d7e1db802e422abb740a0
officel <nishimuray@gmail.com>
2016-08-03 16:40:11 +0900

    env?

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -8,11 +8,11 @@ describe file('/opt/anyenv/envs') do
   it { should be_directory }
 end
 
-describe command('/opt/anyenv/bin/anyenv') do
+describe command('ANYENV_ROOT=/opt/anyenv /opt/anyenv/bin/anyenv') do
   its(:stdout) { should contain('Usage') }
 end
 
-describe command('/opt/anyenv/bin/anyenv versions') do
+describe command('ANYENV_ROOT=/opt/anyenv /opt/anyenv/bin/anyenv versions') do
   its(:stdout) { should contain('rbenv') }
   its(:stdout) { should contain('pyenv') }
 end
